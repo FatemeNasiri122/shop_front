b4a9b1c5317ff7424c3f5b4e4315c10d21f531be
nasiri <futinasiri7@gmail.com>
2024-03-15 16:54:56 +0330

    edit manifest

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,38 @@ export default defineConfig({
   plugins: [react(),
   VitePWA({ 
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      display: "standalone",
       manifest: {
-        name: 'Vite PWA Project',
-        short_name: 'Vite PWA Project',
-        theme_color: '#ffffff',
+        name: 'clothing shop',
+        short_name: 'shop',
+        theme_color: '#1976D2',
         icons: [
             {
                 src: 'pwa-64x64.png',
                 sizes: '64x64',
-                type: 'image/png'
+            type: 'image/png',
+                purpose: "maskable"
             },
             {
                 src: 'pwa-192x192.png',
                 sizes: '192x192',
-                type: 'image/png'
+              type: 'image/png',
+                purpose: "maskable"
             },
             {
                 src: 'pwa-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'any'
+              purpose: 'any',
+                purpose: "maskable"
             },
             {
                 src: 'maskable-icon-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'maskable'
+              purpose: 'maskable',
+                purpose: "maskable"
             }
         ],
       }, 
