68a478dd4fad69b6206c98b62535349010913d3c
nasiri <futinasiri7@gmail.com>
2024-03-16 13:59:34 +0330

    delete pwa

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      display: 'standalone',
-      manifest: {
-        name: 'clothing shop',
-        short_name: 'shop',
-        theme_color: '#1976D2',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-            purpose: 'maskable'
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-            purpose: 'maskable'
-          }
-        ]
-      }
-    })
   ]
 });
